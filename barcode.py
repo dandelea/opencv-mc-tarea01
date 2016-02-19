a34b47c4b4b11e70a6b2f94a04547e8ebe53534f
dandelea <dandelea@alum.us.es>
2016-02-19 11:40:20 +0000

    BUGFIX: Proceso barcode corregido

--- barcode.py
+++ barcode.py
@@ -7,33 +7,19 @@ def run(img_url):
 	img = cv2.imread(img_url)
 	# Convert to gray
 	gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-	# Gaussian blur
-	blur = cv2.GaussianBlur(gray,(5,5), 0)
 	# Simple equalization in order to not lose forward information
-	equ = cv2.equalizeHist(blur)
+	equ = cv2.equalizeHist(gray)
 
-	c = 255/math.log10(256)
-	copy = equ.copy()
-	height, width = copy.shape
-	for i in range(0, height):
-		for j in range(0, width):
-			copy[i, j] = c * math.log10(1 + copy[i, j])
+	# Canny edge 
+	edges = cv2.Canny(equ,100,200)
 
-	scharrx = cv2.Scharr(copy,cv2.CV_64F,1,0)
-	scharry = cv2.Scharr(copy,cv2.CV_64F,0,1)
-	scharr = scharrx + scharry
-
-	plt.subplot(2,3,1),plt.imshow(img,cmap = 'gray')
+	plt.subplot(2,2,1),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB),cmap = 'gray')
 	plt.title('Original'), plt.xticks([]), plt.yticks([])
-	plt.subplot(2,3,2),plt.imshow(gray,cmap = 'gray')
+	plt.subplot(2,2,2),plt.imshow(gray,cmap = 'gray')
 	plt.title('Gray'), plt.xticks([]), plt.yticks([])
-	plt.subplot(2,3,3),plt.imshow(blur,cmap = 'gray')
-	plt.title('Gaussian blur'), plt.xticks([]), plt.yticks([])
-	plt.subplot(2,3,4),plt.imshow(equ,cmap = 'gray')
+	plt.subplot(2,2,3),plt.imshow(equ,cmap = 'gray')
 	plt.title('Equalization'), plt.xticks([]), plt.yticks([])
-	plt.subplot(2,3,5),plt.imshow(copy,cmap = 'gray')
-	plt.title('Log transformation'), plt.xticks([]), plt.yticks([])
-	plt.subplot(2,3,6),plt.imshow(scharr,cmap = 'gray')
-	plt.title('Edge detection'), plt.xticks([]), plt.yticks([])
+	plt.subplot(2,2,4),plt.imshow(edges,cmap = 'gray')
+	plt.title('Canny edge detection'), plt.xticks([]), plt.yticks([])
 
 	plt.show()
